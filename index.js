a9dff95342ab2e39ad3071a863ffd08c504c44a8
Aymankhalif <harithkhalif90@gmail.com>
2023-03-23 10:32:01 +0300

    done with the assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Your code here
+function moveDodgerLeft() {
+  const left = parseInt(dodger.style.left, 10);
+  if (left > 0) {
+    dodger.style.left = `${left - 4}px`;
+  }
+}
+function moveDodgerRight() {
+  const left = parseInt(dodger.style.left, 10);
+  if (left < 360) {
+    dodger.style.left = `${left + 4}px`;
+  }
+}
